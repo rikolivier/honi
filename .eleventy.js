898efe5b148e851215fb3993522aa2b6adc5b278
Rik Olivier <rik.olivier@gmail.com>
2024-02-05 21:47:09 +0100

    Add 11ty Image to the project

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,26 @@
+const Image = require("@11ty/eleventy-img");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("src/assets/");
   eleventyConfig.addPassthroughCopy("src/css/");
+  eleventyConfig.addShortcode("image", async function (src, alt, sizes) {
+    let metadata = await Image(src, {
+      widths: [300, 600],
+      formats: ["avif", "jpeg"],
+    });
+
+    let imageAttributes = {
+      alt,
+      sizes,
+      loading: "lazy",
+      decoding: "async",
+    };
+
+    // You bet we throw an error on a missing alt (alt="" works okay)
+    return Image.generateHTML(metadata, imageAttributes);
+  });
+ 
   return {
     dir: {
       input: "src",
